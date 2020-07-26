067addea78d52e10da4aa7f2fefb1a7655245aab
ChristineWasike <christinewasike22@gmail.com>
2020-07-26 15:30:42 +0300

    Solved

--- src/mastermind/evaluateGuess.kt
+++ src/mastermind/evaluateGuess.kt
@@ -1,11 +1,30 @@
 package mastermind
 
+import java.util.*
+
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun main() {
-    evaluateGuess("abc", "abc")
+    println(evaluateGuess("ABCD", "CDBA"))
 }
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    println(guess)
+    var rightPosition = 0
+    var wrongPosition = 0
+
+    val letterDict = hashMapOf<String, String>()
+
+    for (i in secret.indices) {
+        letterDict[secret[i].toString()] = guess[i].toString()
+    }
+
+    for ((key, value) in letterDict) {
+        if (key == value) {
+            rightPosition +=1
+        } else if(key != value && value in letterDict.keys){
+            wrongPosition +=1
+        }
+    }
+
+    return Evaluation(rightPosition, wrongPosition)
 }
